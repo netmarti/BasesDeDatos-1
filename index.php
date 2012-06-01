90361932193d3ec7154e16e3a22ef03cee03390a
Tomas <tomas@ubuntu.ubuntu-domain>
2012-05-31 23:25:44 -0400

    layouts

--- index.php
+++ index.php
@@ -1,23 +1,6 @@
-<html>
-<head>
 
-</head>
-<body>
-	<h1>Buena buena!</h1>
-	<a href="eventos/listar.php">Eventos</a>
-	<br />
-	<a href="tiendas/listar.php">Tiendas</a>
-	<br />
-	<a href="resultados/listar_resultados.php">Resultados</a>
-	<br />
-	<a href="auspiciadores/listar.php">Auspiciadores</a>
-	<br />
-	<a href="participantes/listar.php">Participantes</a>
-	<br />
-	<a href="inscripciones/listar.php">Inscripciones</a>
-	<br />
-	<a href="productos/listar.php">Productos</a>
-
-<body>
-
-</html>
+<?php include 'layout_above.php';?>
+<div>
+	<p> Esta es la pagina de inicio de Marrel SA, the greates mofos on earth.</p>
+</div>
+<?php include 'layout_bottom.php';?>
