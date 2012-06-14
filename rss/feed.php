3131b2cb8bfe7cac0afb93b5fc9cdbebf5e88851
Cristobal Mackenzie <c.mack04@gmail.com>
2012-06-14 16:33:14 -0400

    subo rss feed

--- rss/feed.php
+++ rss/feed.php
@@ -1,7 +1,9 @@
 <?php
 //hay que ir a buscar un xml a algun feed RSS y imprimirlo en un formato cool.
+$xml = file_get_contents("http://www.sport.es/es/rss/last_news/rss.xml");
+$res = new SimpleXMLElement($xml);
 
-
+//die($res->channel->item[0]->title);
 ?>
 <html>
 <head>
@@ -11,7 +13,14 @@
 <?php include "../navbar.php"?>
 <body>
 	<div class="container">
-		Este es nuestro feed RSS
+		<p>Todas las noticias del mundo del deporte las tienes aqui!</p>
+		<?php foreach ($res->channel->item as $item) {?>
+		<p>
+			<h4><?php echo $item->title?></h4> <br />
+		<!-- <a href=<?php echo $item->link?>>Fuente</a>-->
+			<?php echo $item->description?>
+		</p>
+		<?php }?>
 	</div>
 </body>
 </html>
