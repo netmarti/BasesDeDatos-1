d3588bcf3569f3a4b46cf47f0bdff2f80a9995d9
Cristobal Mackenzie <c.mack04@gmail.com>
2012-06-14 16:04:04 -0400

    subo php para feed rss

---
+++ rss/feed.php
@@ -0,0 +1,17 @@
+<?php
+//hay que ir a buscar un xml a algun feed RSS y imprimirlo en un formato cool.
+
+
+?>
+<html>
+<head>
+	<link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
+	<title>RSS Feed Marrel</title>
+</head>
+<?php include "../navbar.php"?>
+<body>
+	<div class="container">
+		Este es nuestro feed RSS
+	</div>
+</body>
+</html>
