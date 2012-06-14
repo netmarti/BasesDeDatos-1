14b7271688ad1762fe6e88f8974019fcee6d84fa
Tomas Dussaillant <tomas@ubuntu.ubuntu-domain>
2012-06-14 12:11:14 -0400

    m

--- db_connect.php
+++ db_connect.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-	$db = new PDO("pgsql:dbname=proyecto host=localhost port=5432 user=grupo14 password=basesdedatos");
+	$db = new PDO("pgsql:dbname=proyecto host=localhost port=5432 user=postgres password=0963312j");
 }
 catch(PDOException $e) {
 	die ('Problema al conectar a la base de datos!');
